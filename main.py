e332757f41e83b7ae1d1f2fbd13393f6a643c99d
Mikhail Kutsov <aaaaa@a.ru>
2024-09-23 19:39:34 +0500

    crete folders

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 def generate_folders():
     for num in range(1, 22):
         os.mkdir("mod-{}".format(num))
+        open('mod-{}/test'.format(num), 'w').write('www')
 
 
 if __name__ == "__main__":
     generate_folders()
-
